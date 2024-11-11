7fbded9b4c09b08c1093440d5a45d32d3d09f50a
Trencsánszky Patrik <trencsanszkyp@kkszki.hu>
2024-11-11 22:29:49 +0100

    javitott ules scirpt

--- Assets/scripts/Sit.cs
+++ Assets/scripts/Sit.cs
@@ -2,23 +2,26 @@ using UnityEngine;
 
 public class Sit : MonoBehaviour, IInteractable
 {
-    public Transform SitPosition;
-    public bool isSitting = false;
+    public Transform SitPosition; 
+    public Transform StandPosition; 
+    public bool isSitting = false; 
+
+    private GameObject player;
+    private Movement playerMovementScript;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerMovementScript = player.GetComponent<Movement>();
+    }
 
     public void Interact()
     {
-        GameObject player = GameObject.FindWithTag("Player");
-
-        // Mozgásvezérlő script lekérése
-        var playerMovementScript = player.GetComponent<Movement>(); // Cseréld ki a saját mozgás scripted nevére
-
         if (isSitting == false)
         {
-            // Karakter leültetése
             player.transform.position = SitPosition.position;
             player.transform.rotation = SitPosition.rotation;
 
-            // Mozgás letiltása és állapot frissítése
             if (playerMovementScript != null)
             {
                 playerMovementScript.enabled = false;
@@ -27,12 +30,21 @@ public class Sit : MonoBehaviour, IInteractable
         }
         else
         {
-            // Karakter felállítása
+            player.transform.position = StandPosition.position; 
+            player.transform.rotation = StandPosition.rotation;
             if (playerMovementScript != null)
             {
                 playerMovementScript.enabled = true;
             }
             isSitting = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isSitting && Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
         }
     }
 }
