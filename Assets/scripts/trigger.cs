7c27e8ef922e8ecacbe838fe52cdc0558c80e139
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2024-11-22 09:30:29 +0100

    trig

---
+++ Assets/scripts/trigger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class hangeffektek : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+
+            
+        }
+    }
+}
