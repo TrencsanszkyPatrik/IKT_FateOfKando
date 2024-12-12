3e56055c2431710775c783b97fa5c0963c9b8631
Trencsánszky Patrik <trencsanszkyp@kkszki.hu>
2024-12-12 13:52:43 +0100

    félig működő cigitekerés

--- Assets/scripts/MINIGAMES/Rolling.cs
+++ Assets/scripts/MINIGAMES/Rolling.cs
@@ -1,19 +1,92 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using System.Collections;
+using jatek;
 
-public class Rolling : MonoBehaviour
+public class Roller : MonoBehaviour
 {
-    public Button gomb;
+    public GameObject cigaretteBox;
+    public GameObject cigarettePaper;
+    public GameObject tobacco;
+    public Slider rollingSlider;
+    public Button lickButton;
+    public GameObject completedMessage;
+
+    private bool isPaperTaken = false;
+    private bool isTobaccoDragged = false;
+    private bool isRollingStarted = false;
+    private bool isLicked = false;
+    private RectTransform draggedObject;
+    private Vector2 originalPosition;
+    private DragAndDrop tobaccoDragAndDrop;
 
     void Start()
     {
-        Button btn = gomb.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
+        cigarettePaper.SetActive(false);
+        lickButton.gameObject.SetActive(false);
+        completedMessage.SetActive(false);
+        rollingSlider.gameObject.SetActive(false);
+
+        tobaccoDragAndDrop = tobacco.GetComponent<DragAndDrop>();
     }
 
-    void TaskOnClick()
+    void Update()
     {
-        Debug.Log("You have clicked the button!");
+        CheckTobaccoPlacement();
+    }
+
+    public void OnCigaretteBoxClicked()
+    {
+        if (!isPaperTaken)
+        {
+            cigarettePaper.SetActive(true);
+            isPaperTaken = true;
+            tobacco.SetActive(true);
+        }
+    }
+
+    public void OnLickButtonPressed()
+    {
+        if (!isLicked)
+        {
+            isLicked = true;
+            lickButton.gameObject.SetActive(false);
+            rollingSlider.gameObject.SetActive(true);
+            StartRolling();
+        }
+    }
+
+    public void OnRollingSliderChanged()
+    {
+        if (isRollingStarted)
+        {
+        }
+    }
+
+    private void StartRolling()
+    {
+        if (isLicked)
+        {
+            isRollingStarted = true;
+            rollingSlider.gameObject.SetActive(true);
+            completedMessage.SetActive(false);
+        }
+    }
+
+    private void CheckTobaccoPlacement()
+    {
+        if (tobaccoDragAndDrop != null && tobaccoDragAndDrop.IsPlaced())
+        {
+            if (!isTobaccoDragged)
+            {
+                isTobaccoDragged = true;
+                lickButton.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public bool IsPaperAvailable()
+    {
+        return cigarettePaper.activeSelf;
     }
 }
